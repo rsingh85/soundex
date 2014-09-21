24671f61027afb87239a484cb5668e853f9ee0d2
Ravi Singh <rsingh85@users.noreply.github.com>
2014-09-21 15:04:14 +0100

    Soundex algorithm in C# initial commit.

---
+++ Soundex.Tests/SoundexTest.cs
@@ -0,0 +1,109 @@
+﻿using Soundex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Soundex.Tests
+{
+    /// <summary>
+    ///This is a test class for Soundex and is intended
+    ///to contain all Soundex Unit Tests
+    ///</summary>
+    [TestClass]
+    public class SoundexTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        /// <summary>
+        /// A test for the Soundex.For method. Runs a series of words through
+        /// Soundex.For and asserts that their output is correct.
+        ///</summary>
+        [TestMethod]
+        public void ForTests()
+        {
+            string word, expected, actual;
+
+            word = "";
+            expected = "0000";
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+
+            word = null;
+            expected = "0000";
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+
+            word = "Robert";
+            expected = "R163";
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+
+            word = "Rupert".ToLower();
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+
+            word = "Rubin";
+            expected = "R150";
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+
+            word = "Ashcraft";
+            expected = "A261";
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+
+            word = "Ashcroft";
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+
+            word = "Tymczak";
+            expected = "T522";
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+
+            word = "Pfister";
+            expected = "P236";
+            actual = Soundex.For(word);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Soundex/Soundex.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Soundex
+{
+    /// <summary>
+    /// Provides functionality to retrieve a soundex code for a given word.
+    /// </summary>
+    public static class Soundex
+    {
+        /// <summary>
+        /// Returns the soundex code for a specified word.
+        /// </summary>
+        /// <param name="word">Word to get the soundex for.</param>
+        /// <returns>Soundex code for word.</returns>
+        public static string For(string word)
+        {
+            const int MaxSoundexCodeLength = 4;
+
+            var soundexCode = new StringBuilder();
+            var previousWasHOrW = false;
+
+            // Upper case all letters in word and remove any punctuation
+            word = Regex.Replace(
+                word == null ? string.Empty : word.ToUpper(),
+                    @"[^\w\s]", 
+                        string.Empty);
+
+            if (string.IsNullOrEmpty(word)) 
+                return string.Empty.PadRight(MaxSoundexCodeLength, '0');
+
+            // Retain the first letter
+            soundexCode.Append(word.First());
+
+            for (var i = 1; i < word.Length; i++)
+            {
+                var numberCharForCurrentLetter = GetCharNumberForLetter(word[i]);
+
+                // Skip this number if it matches the number for the first character
+                if (i == 1 && 
+                        numberCharForCurrentLetter == GetCharNumberForLetter(soundexCode[0]))
+                    continue;
+
+                // Skip this number if the previous letter was a 'H' or a 'W' 
+                // and this number matches the number assigned before that
+                if (soundexCode.Length > 2 && previousWasHOrW && 
+                        numberCharForCurrentLetter == soundexCode[soundexCode.Length - 2])
+                    continue;
+
+                // Skip this number if it was the last added
+                if (soundexCode.Length > 0 && 
+                        numberCharForCurrentLetter == soundexCode[soundexCode.Length - 1])
+                    continue; 
+
+                soundexCode.Append(numberCharForCurrentLetter);
+
+                previousWasHOrW = "HW".Contains(word[i]);
+            }
+
+            return soundexCode
+                    .Replace("0", string.Empty)
+                        .ToString()
+                            .PadRight(MaxSoundexCodeLength, '0')
+                                .Substring(0, MaxSoundexCodeLength);
+        }
+
+        /// <summary>
+        /// Retrieves the soundex number for a given letter.
+        /// </summary>
+        /// <param name="letter">Letter to get the soundex number for.</param>
+        /// <returns>Soundex number (as a character) for letter.</returns>
+        private static char GetCharNumberForLetter(char letter)
+        {
+            if ("BFPV".Contains(letter)) return '1';
+            if ("CGJKQSXZ".Contains(letter)) return '2';
+            if ("DT".Contains(letter)) return '3';
+            if ('L' == letter) return '4';
+            if ("MN".Contains(letter)) return '5';
+            if ('R' == letter) return '6';
+
+            return '0'; // i.e. letter is [AEIOUWYH]
+        }
+    }
+}
